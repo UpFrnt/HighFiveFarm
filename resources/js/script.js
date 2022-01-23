b4dd11c44022b1fee5b9cbfbd380653df8b5442d
Alyson Williams <alysonrwilliams@gmail.com>
2022-01-23 09:57:57 -0500

    nav bar media query fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    
-    /*For the sticky Navigation*/
-    $('.js--section-features').waypoint(function(direction) {
+
+  /* For the sticky navigation */
+  $('.js--section-features').waypoint(function(direction) {
       if (direction == "down") {
           $('nav').addClass('sticky');
       } else {
@@ -11,8 +11,65 @@ $(document).ready(function() {
   }, {
     offset: '60px;'
   });
+  
+  /* Scroll on buttons */
+    
+  $('.js--scroll-to-plans').click(function(){
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
 
-      /*Mobile Navigation*/
+});
+
+ $('.js--scroll-to-start').click(function(){
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+});
+
+/* Navigation scroll* */
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        console.log('test');
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+          console.log('test2');
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+          console.log('test3');
+        return false;
+      }
+    }
+  });
+});
+
+/* Animations on scroll */
+
+$('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+}, {
+    offset: '50%'
+});
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+}, {
+    offset: '50%'
+});
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+}, {
+    offset: '50%'
+});
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+}, {
+    offset: '50%'
+});
+  
+   
+  
+  /*Mobile Navigation*/
       $('.js--nav-icon').click(function(){
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
